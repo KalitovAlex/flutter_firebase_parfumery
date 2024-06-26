556fbc88beb6412321cf88aa0bc37fb4094786f0
rsgamertv <Wersuser2@gmail.com>
2024-06-26 20:01:41 +0300

    "Refactor BottomNavigation class from StatelessWidget to StatefulWidget, update tabs configuration and navigation bar styling"

--- lib/main/bottom_navigation.dart
+++ lib/main/bottom_navigation.dart
@@ -3,44 +3,39 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_parfumery/core/routes/routes.gr.dart';
 import 'package:flutter_firebase_parfumery/main/models/recomendation/recommendation.dart';
-import 'package:flutter_firebase_parfumery/main/screen/bucket_screen.dart';
-import 'package:flutter_firebase_parfumery/main/screen/favorite_screen.dart';
-import 'package:flutter_firebase_parfumery/main/screen/home_screen.dart';
-import 'package:flutter_firebase_parfumery/main/screen/profile_screen.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
+import 'package:sizer/sizer.dart';
 
 
 @RoutePage()
-class BottomNavigation extends StatelessWidget {
+class BottomNavigation extends StatefulWidget {
+
   BottomNavigation({super.key, required this.response});
   List<Recommendation> response; 
 
-  List<PersistentTabConfig> _tabs() => [
-        PersistentTabConfig(
-          screen: HomeScreen(response: response,),
-          item: ItemConfig(
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+
+  List<ItemConfig> _tabs() => [
+          ItemConfig(
             inactiveIcon: const Icon(CupertinoIcons.compass,color: Colors.white,),
             icon: const Icon(CupertinoIcons.compass_fill,color: Colors.white,),
-          ),
         ),
-        PersistentTabConfig(
-          screen: const BucketScreen(),
-          item: ItemConfig(
+        ItemConfig(
             inactiveIcon: const Icon(CupertinoIcons.cart,color: Colors.white,),
             icon: const Icon(CupertinoIcons.cart_fill,color: Colors.white,),
           ),
-        ),
-        PersistentTabConfig(
-          screen: const FavoriteScreen(),
-          item: ItemConfig(
+          ItemConfig(
             inactiveIcon: const Icon(Icons.favorite_outline,color: Colors.white,),
             icon: const Icon(Icons.favorite,color: Colors.white,),
           ),
-        ),
-        PersistentTabConfig(screen: const Scaffold(), item: ItemConfig(
+          ItemConfig(
           inactiveIcon: const Icon(CupertinoIcons.doc,color: Colors.white,),
-          icon: const Icon(CupertinoIcons.doc_fill,color: Colors.white,))),
-        PersistentTabConfig(screen: const ProfileScreen(), item: ItemConfig(icon: const Icon(CupertinoIcons.person_fill,color: Colors.white,),inactiveIcon: const Icon(CupertinoIcons.person, color: Colors.white,)))
+          icon: const Icon(CupertinoIcons.doc_fill,color: Colors.white,)),
+          ItemConfig(icon: const Icon(CupertinoIcons.person_fill,color: Colors.white,),inactiveIcon: const Icon(CupertinoIcons.person, color: Colors.white,))
       ];
 
   @override
@@ -49,25 +44,30 @@ class BottomNavigation extends StatelessWidget {
     canPop: false,
     child: AutoTabsRouter(
       routes: [
-        HomeRoute(response: response),
+        HomeRoute(response: widget.response),
         const BucketRoute(),
         const FavoriteRoute(),
         const HistoryRoute(),
         const ProfileRoute(),
       ],
       builder: (context, child) {
-        return PersistentTabView(
-          margin: const EdgeInsets.only(bottom: 1,left: 15,right: 15),
-          avoidBottomPadding: true,
-          navBarHeight: 70,
-          resizeToAvoidBottomInset: true,
-          tabs: _tabs(),
-          navBarBuilder: (navBarConfig) => Style1BottomNavBar(
-            navBarDecoration: const NavBarDecoration(borderRadius: BorderRadius.only(topLeft: Radius.circular(15),topRight: Radius.circular(15),bottomRight: Radius.circular(15) ,bottomLeft: Radius.circular(15)),color: Colors.green),
-            navBarConfig: navBarConfig,
+        final tabsRouter = AutoTabsRouter.of(context);
+        return Scaffold(
+          bottomNavigationBar:  Card(
+            color: Colors.transparent,
+            margin: EdgeInsets.only(bottom: 2.h, left: 5.w, right: 5.w),
+            child: Style1BottomNavBar(
+                  navBarDecoration:  NavBarDecoration(padding: EdgeInsets.only(top: 3.h),borderRadius: const BorderRadius.only(topLeft: Radius.circular(15),topRight: Radius.circular(15),bottomRight: Radius.circular(15) ,bottomLeft: Radius.circular(15)),color: Colors.green),
+                  navBarConfig: NavBarConfig(selectedIndex: tabsRouter.activeIndex, items: _tabs(), onItemSelected: (index) => openPage(index,tabsRouter)),
+                ),
           ),
+          body: child,
         );
       },
     )
   );}
+
+  void openPage(int index, TabsRouter tabRouter){
+    tabRouter.setActiveIndex(index);
+  }
 }
